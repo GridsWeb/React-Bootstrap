f310f28d70aca67b52758908ee134d0cbe15813a
J.Grids <88521380+GridsETH2@users.noreply.github.com>
2022-02-28 22:54:59 +0700

    Update App.js

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -4,21 +4,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Nav from 'react-bootstrap/Nav';
-import NavBar from 'react-bootstrap/NavBar';
+import NavBar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Cal from 'react-bootstrap/Cal';
+import Col from 'react-bootstrap/Col';
 
 import { BrowserRouter as Router, Switch, Rourte, Link } from 'react-router-dom';
 
-import File from './components/file.component'
 import CreateUser from './components/create-user.component'
 import EditUser from './components/edit-user.component'
 import UserList from './components/user-list.component'
 
 function App() {
   return (
-    <Rourte>
+    <Router>
       <div className="App">
         <header className="App-header">
 
@@ -92,7 +91,7 @@ function App() {
         </Container>
 
       </div>
-    </Rourte>
+    </Router>
   );
 }
 
